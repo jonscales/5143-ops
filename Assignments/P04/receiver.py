59d7dc7a644ea922e9a0704c33472537f1cd1d4a
jonscales <jon.scales@msutexas.edu>
2023-12-04 17:40:06 -0600

    incorporated Pats code into reciever.py

--- Assignments/P04/receiver.py
+++ Assignments/P04/receiver.py
@@ -53,17 +53,62 @@ def usage():
     print("Usage: receiver.py <host> <port> <exchange> <routing_keys> ")
     print("Usage: receiver.py 164.90.134.137 5672 cpuproject 'sports,news' ")
     sys.exit()
+RegD={"R1":0,"R2":0,"R3":0,"R4":0,"R5":0,"R6":0}
+def writereg(loc,val):
+    RegD[loc]=val
 
 def processMessage(ch, method, properties, body):
         #print(f"I will not beat off any more")
         data = json.loads(body.decode())
         outfile="messagedata.dat"
-        with open(outfile, 'w',) as outfile:
-            for instructblock in data1:
+        with open(outfile, 'a',) as outfile: #Append new messages to existing file. 
+            for instructblock in data:
                 for line in instructblock:
                     print(line)
                     outfile.write(line +'\n')
-                
+
+        for i in data:
+            for t in range(len(data[0])):
+                single=i[t].split()
+                if single[0]=="LOAD":
+                    writereg(single[1],single[2])
+                if single[0]=="ADD":
+                    #print(single[1])
+                    #print(RegD[single[1]])
+                    writereg(single[1],int(RegD[single[1]]) + int(RegD[single[2]]))
+                if single[0]=="DIV":
+                    writereg(single[1],int(RegD[single[1]]) / int(RegD[single[2]]))
+                if single[0]=="SUB":
+                    writereg(single[1],abs(int(RegD[single[1]]) - int(RegD[single[2]])))
+                if single[0]=="MUL":
+                    writereg(single[1],int(RegD[single[1]]) * int(RegD[single[2]]))
+                if single[0]=="STORE":
+                    rgb=single[1]
+                    rgb2=rgb.replace('(','').replace(')','')
+                    rgb3=rgb2.split(',')
+                    # print("This print= ",rgb3)
+                    # print("this print 1= ",rgb3[0])
+                    # print("this print 2= ",rgb3[1])
+                    # print("this print 3= ",rgb3[2])
+                    xy=single[2]
+                    #print(xy)
+                    xy2=xy.replace('(','').replace(')','')
+                    xy3=xy2.split(',')
+                    # print("this is x ",xy3[0])
+                    # print("this is y ",xy3[1])
+                    print(f'"STORE" : [{RegD[rgb3[0]]},{RegD[rgb3[1]]},{RegD[rgb3[2]]}],"xy": [ {RegD[xy3[0]]},{RegD[xy3[1]]} ]   ')
+                    #print(RegD[rgb3[0]])for instructblock in data:
+                    
+            print(RegD)
+            print("\n")  
+
+
+
+        
+            
+        
+   
+                  
                
 
 
